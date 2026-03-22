49f311c118d4893f72a634c2bd0031c3874276ca
DalilahH1 <106951589+MajesticL@users.noreply.github.com>
2026-03-21 22:31:46 -0400

    Add json

---
+++ src/JS/model.js
@@ -0,0 +1,34 @@
+const state = [
+  (columns = [
+    {
+      id: 1,
+      title: "TO-DO",
+      task: {
+        id: 1,
+        content: "Start planning",
+        date: "3/10/2026",
+        priority: "high",
+      },
+    },
+    {
+      id: 2,
+      title: "In Progress",
+      task: {
+        id: 1,
+        content: "Start planning",
+        date: "3/14/2026",
+        priority: "high",
+      },
+    },
+    {
+      id: 3,
+      title: "Done",
+      task: {
+        id: 1,
+        content: "Start planning",
+        date: "3/20/2026",
+        priority: "low",
+      },
+    },
+  ]),
+];
